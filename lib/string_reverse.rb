7a7259a46c31bc27d65250e002143670b2e442aa
Alexandria Brown <axbn2190@gmail.com>
2018-03-20 20:29:28 -0700

    fixed nil string outcome

--- lib/string_reverse.rb
+++ lib/string_reverse.rb
@@ -1,4 +1,20 @@
 # A method to reverse a string in place.
 def string_reverse(my_string)
-  my_string << "not implemented"
+  if my_string == nil
+    return nil
+  end
+
+  i = 0
+  j = my_string.length - 1
+
+  while i < j
+    temp = my_string[i]
+    temp2 = my_string[j]
+
+    my_string[i] = temp2
+    my_string[j] = temp
+
+    i += 1
+  	j -= 1
+  end
 end
