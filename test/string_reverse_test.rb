c42af62fd0984322cac59f5d45805c85b2be445f
Chris M <chris@adadevelopersacademy.org>
2019-09-09 17:02:29 -0700

    Added minitest reporters

--- test/string_reverse_test.rb
+++ test/string_reverse_test.rb
@@ -2,6 +2,8 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 require_relative '../lib/string_reverse'
 
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
 describe "string reverse" do
   describe "basic tests" do
     it "reverse a string with odd number of characters" do
